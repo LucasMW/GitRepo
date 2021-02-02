3d11bf7a4e81db3540000f99d20652c0669ba266
LucasMW <great-lucas@hotmail.com>
2021-02-02 20:54:39 +0000

    make it work in old iOS 10.0

--- GitRepo/AppDelegate.swift
+++ GitRepo/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyBoard.instantiateInitialViewController()
+        self.window?.rootViewController = vc
+
         return true
     }
 
